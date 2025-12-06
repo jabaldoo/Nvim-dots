ce7fcb1f41e81ca37b17f8ccaf6f356ccd9802fc
jabaldoo <michalmatera9@gmail.com>
2025-12-06 23:16:52 +0100

    language support and nothing else

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "python", "asm", "swift", "java", "css" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
